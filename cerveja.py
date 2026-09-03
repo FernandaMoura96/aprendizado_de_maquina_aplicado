a9dd916cff362caecee2c918a7d03826ef5db84e
Fernanda Moura <ferferreira199609@gmail.com>
2026-09-03 13:41:25 -0300

    Arvore de decisão

---
+++ cerveja.py
@@ -0,0 +1,35 @@
+#%%
+import pandas as pd 
+from sklearn import tree
+import matplotlib.pyplot as plt 
+
+df = pd.read_excel("C:/Users/nanda/OneDrive/Desktop/machine/data/dados_cerveja.xlsx")
+df
+# %%
+
+features = ['temperatura','copo','espuma','cor']
+target = 'classe'
+
+X = df[features]
+y = df[target]
+X = X.replace( {"mud":1, 'pint' :2,
+          'sim' :1, 'não':0,
+          'clara':0 , 'escura':1
+          })
+# %%
+
+
+model = tree.DecisionTreeClassifier()
+
+model.fit(X = X , y=y )
+
+# %%
+
+
+plt.figure(dpi=400)
+
+tree.plot_tree(model, feature_names=features,
+               class_names=model.classes_,
+               filled=True 
+               )
+# %%
